8e4a683704b1112941400422252a022199d655e8
Seblis <seblis91@gmail.com>
2022-10-30 01:36:54 +0200

    Empty django project created

---
+++ dbd_buildmaker/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dbd_buildmaker project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dbd_buildmaker.settings')
+
+application = get_asgi_application()

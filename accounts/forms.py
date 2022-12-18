f71cbed8fb9c1ceb1b164c301359c62904be0332
Seblis <seblis91@gmail.com>
2022-12-18 01:06:42 +0100

    Label tag used
    Also first Black formatter test

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm, UsernameField
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    AuthenticationForm,
+    UsernameField,
+)
 from django.contrib.auth import get_user_model
 from django.forms import TextInput, CharField, PasswordInput
+
 
 class BuildmakerUserCreationForm(UserCreationForm):
     class Meta:
@@ -19,20 +25,27 @@ class BuildmakerUserChangeForm(UserChangeForm):
             'email',
         )
 
+
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.label_suffix = ''
 
-    username = UsernameField(widget=TextInput(
-        attrs={
-            'class': 'form-control',
-            'placeholder': 'Username',
-            'id': 'floatingInput'}
-    ))
-    password = CharField(widget=PasswordInput(
-        attrs={
-            'class': 'form-control',
-            'placeholder': 'Password',
-            'id': 'floatingPassword',
-        }
-    ))
+    username = UsernameField(
+        widget=TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Username',
+                'id': 'floatingInput',
+            }
+        )
+    )
+    password = CharField(
+        widget=PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password',
+                'id': 'floatingPassword',
+            }
+        )
+    )
